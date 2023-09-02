744e16651eb9674de63e9f5b9dbba03e521de427
Anmol Arora <anmol.arora8991@gmail.com>
2023-09-02 05:40:04 -0400

    resolved timzone issue

--- src/views/dashboard/user/components/Timer.jsx
+++ src/views/dashboard/user/components/Timer.jsx
@@ -65,24 +65,20 @@ const Timer = () => {
           if(response?.data?.data?.entryTime !== null){
             const entryTimeParts = response?.data?.data?.entryTime.split(":");
             const currentUTC = new Date(); // Get the current date and time in UTC
-
             // Construct the entryTime object in UTC/GMT timezone
-            const entryTime = new Date(
-              currentUTC.getUTCFullYear(),
-              currentUTC.getUTCMonth(),
-              currentUTC.getUTCDate(),
-              parseInt(entryTimeParts[0]) - 4, // Subtract 4 hours for GMT
-              parseInt(entryTimeParts[1]),
-              parseInt(entryTimeParts[2])
-            );
+            const entryTime = new Date(currentUTC.getUTCFullYear(),currentUTC.getUTCMonth(), currentUTC.getUTCDate(),);
+            entryTime.setHours(parseInt(entryTimeParts[0]));
+            entryTime.setMinutes(parseInt(entryTimeParts[1]));
+            entryTime.setSeconds(parseInt(entryTimeParts[2]));
 
-            console.log("Entry Time (GMT): ", entryTime.toUTCString());
+            console.log("Entry Time (GMT): ", entryTime);
+            console.log("Current Time (GMT): ", currentUTC.toUTCString());
 
             // Calculate the time difference in seconds
             const entryTimeInSeconds =
-              entryTime.getUTCHours() * 3600 +
-              entryTime.getUTCMinutes() * 60 +
-              entryTime.getUTCSeconds();
+              entryTime.getHours() * 3600 +
+              entryTime.getMinutes() * 60 +
+              entryTime.getSeconds();
 
             console.log("Entry Time in Seconds (GMT): ", entryTimeInSeconds);
 
@@ -93,7 +89,7 @@ const Timer = () => {
 
             console.log("Current Time in Seconds (GMT): ", currentTimeInSeconds);
 
-            const timeDifferenceInSeconds = currentTimeInSeconds - entryTimeInSeconds;
+            timeDifferenceInSeconds = currentTimeInSeconds - entryTimeInSeconds;
             console.log("Time Difference in Seconds (GMT): ", timeDifferenceInSeconds);
           }
           console.log("Total Time in seconds: ",totalTime);
